47be5b687e4ddc49280bccf53141ce01395dc1ec
Bonelli, Lyndsey <lyndsey.bonelli@zuehlke.com>
2022-02-24 22:09:47 +0100

    Fix Skill.

--- src/main/java/com/wildezhawer/hashcode/service/BestProjectMatchFinder.java
+++ src/main/java/com/wildezhawer/hashcode/service/BestProjectMatchFinder.java
@@ -2,16 +2,16 @@ package com.wildezhawer.hashcode.service;
 
 import com.wildezhawer.hashcode.model.Contributor;
 import com.wildezhawer.hashcode.model.Project;
+import com.wildezhawer.hashcode.model.Skill;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class BestProjectMatchFinder {
     // Returns contributor for each role, sorted by role (i.e. entry 0 is for role 0 etc.)
     public static List<Contributor> findBestMatchesForProject(Project project, List<Contributor> potentialContributors) {
         List<Contributor> bestMatches = new ArrayList<>();
-        for (String skill : project.roles.keySet()) {
-            int requiredSkillLevel = project.roles.get(skill);
+        for (Skill skill : project.roles) {
+            int requiredSkillLevel = skill.level;
             int bestDistanceToSkill = -2; // people with 2 or more skill points below requirement aren't candidates
             Contributor bestMatch = null;
             Contributor secondBestMatch = null;
@@ -21,7 +21,7 @@ public class BestProjectMatchFinder {
                     continue;
                 }
 
-                int currentSkillLevel = c.skills.getOrDefault(skill, 0);
+                int currentSkillLevel = c.skills.getOrDefault(skill.name, 0);
                 if (currentSkillLevel < requiredSkillLevel) {
                     continue;
                 }
@@ -36,7 +36,7 @@ public class BestProjectMatchFinder {
                     secondBestMatch = c;
                 }
 
-                if (currentSkillLevel < secondBestMatch.skills.getOrDefault(skill, 0)) {
+                if (currentSkillLevel < secondBestMatch.skills.getOrDefault(skill.name, 0)) {
                     secondBestMatch = c;
                 }
 
@@ -52,7 +52,8 @@ public class BestProjectMatchFinder {
             }
 
             bestMatches.add(bestMatch);
-        }
+            }
+
 
         return bestMatches;
     }
