64b4745c24f299619a4feba258bd04d973dec382
steb <stefan.boesch@zuehlke.com>
2022-02-24 19:17:43 +0100

    models

---
+++ src/main/java/com/wildezhawer/hashcode/model/Contributor.java
@@ -0,0 +1,9 @@
+package com.wildezhawer.hashcode.model;
+
+import java.util.Map;
+
+public class Contributor {
+    public String name;
+    public Map<String, Integer> skills;
+    public Contributor mentoring;
+}
---
+++ src/main/java/com/wildezhawer/hashcode/model/Project.java
@@ -0,0 +1,12 @@
+package com.wildezhawer.hashcode.model;
+
+import java.util.Map;
+
+public class Project {
+
+    public String name;
+    public int duration;
+    public int score;
+    public int bestBefore;
+    public Map<String, Integer> roles;
+}
