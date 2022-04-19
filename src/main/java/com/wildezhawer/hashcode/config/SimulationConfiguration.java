a45aa7f59d9ef27961464f9bf9fce15f939beee0
Bonelli, Lyndsey <lyndsey.bonelli@zuehlke.com>
2022-04-19 13:33:20 +0200

    Add description.

--- src/main/java/com/wildezhawer/hashcode/config/SimulationConfiguration.java
+++ src/main/java/com/wildezhawer/hashcode/config/SimulationConfiguration.java
@@ -1,5 +1,14 @@
 package com.wildezhawer.hashcode.config;
 
+/*
+    File sizes:
+    - a: 3 contributors, 3 projects, 17 lines
+    - b: 50 contributors, 100 projects, 733 lines
+    - c: 1500 contributors, 1000 projects, 46'315 lines
+    - d: 500 contributors, 1000 projects, 14'970 lines
+    - e: 800 contributors, 10'000 projects, 88'350 lines
+    - f: 1000 contributors, 19'413 projects, 503'724 lines
+ */
 public class SimulationConfiguration {
     public final static String[] FILES_TO_READ = {"a_an_example.in.txt", "b_better_start_small.in.txt", "c_collaboration.in.txt", "d_dense_schedule.in.txt", "e_exceptional_skills.in.txt", "f_find_great_mentors.in.txt"};
 }
